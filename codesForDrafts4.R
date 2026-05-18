ae5376302fdfdc451079eff30b71b915ac7fc2b7
lallen-student <lallendelice@umass.edu>
2026-05-18 05:35:18 -0400

    edits

--- codesForDrafts4.R
+++ codesForDrafts4.R
@@ -229,7 +229,7 @@ original_plot
 
 improved_plot <-  original_plot + 
   labs(
-    title = "Average Output Safety Scores by City and Dimension",
+    title = "Safety Scores by City and Dimension",
     fill = "Average Score",
     caption = "Source: safeCities_ecoUI.xlsx") +
   theme_minimal() +
